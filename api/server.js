30e3eeb0960faa7e8c96be76dd535fae9c18307b
Badmus_Farouk1 <72664875+Art101-ui@users.noreply.github.com>
2023-04-11 12:41:03 +0100

    add expressjs

--- api/server.js
+++ api/server.js
@@ -2,7 +2,11 @@
 // const app = express();
 // const cors = require('cors')
 const nodemailer = require('nodemailer');
+var cors = require('cors')
+var app = express()
 require('dotenv').config();
+ 
+app.use(cors())
 
 
 // const PORT = process.env.PORT || 5000
@@ -89,17 +93,8 @@ require('dotenv').config();
 // pass:'kvowmiaahpwvwzff'
 
 export default async (req,res)=>{
-
-
     res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
 
     if(req.method == 'GET'){
         res.send({status:"Welcome to Nodemailer API"})
