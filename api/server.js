794b62f13b34d6af3f59398540e1d8bfeeecc8a6
Badmus_Farouk1 <72664875+Art101-ui@users.noreply.github.com>
2023-04-11 14:44:29 +0100

    new update

--- api/server.js
+++ api/server.js
@@ -6,69 +6,138 @@ require('dotenv').config();
  
 app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send({status:"Welcome to Nodemailer API"})
-})
 
-app.post('/',cors(),async (req,res)=>{
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-    
-        let config={
-            host: 'smtpout.secureserver.net',
-      port: 587,
-      secure: false, // true for 465, false for other ports
-     
-       tls:{},
-            auth:{
-                // user: "info@zara-space.co.uk",
-                // pass:"D=gM3h$9=k"
-                
-                user: process.env.EMAIL, // generated ethereal user
-                pass: process.env.PASSWORD, 
-                
-            }
-        }
-    
-        let transporter = nodemailer.createTransport(config);
-    
-        let mailbody = `
-        <div>
-        <p>FullName: ${req.body.name} </p>
-        <p>Email : ${req.body.email}</p>
-        <p>Phone Number: ${req.body.phone_no}</p>
-        <p>Postcode: ${req.body.post_code}</p>
-        <p>Social media: ${req.body.social_media}</p>
-        <p>Message: ${req.body.message}</p>
-      </div>
-        `
-    
-        const mailOptions = {
-            from: `${process.env.EMAIL}`,
-            // ,
-            to:'obadmus912@gmail.com',
-            subject:` ${req.body.subject}`,
-            html:mailbody,
-            
-            
-        };
-    
-       
+
+const allowCors = fn => async(req,res)=>{
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+        if(req.method == 'GET'){
+            res.send({status:"Welcome to Nodemailer API"})
+
+        }else{
+            let config={
+                host: 'smtpout.secureserver.net',
+        port: 587,
+        secure: false, // true for 465, false for other ports
         
-            try {
-                const info = await transporter.sendMail(mailOptions);
-                console.log('Email Sent' + info.response)
-                
-            } catch (error) {
-                console.log(error);
-                
-                
+        tls:{},
+                auth:{
+                    // user: "info@zara-space.co.uk",
+                    // pass:"D=gM3h$9=k"
+                    
+                    user: process.env.EMAIL, // generated ethereal user
+                    pass: process.env.PASSWORD, 
+                    
+                }
             }
+        
+            let transporter = nodemailer.createTransport(config);
+        
+            let mailbody = `
+            <div>
+            <p>FullName: ${req.body.name} </p>
+            <p>Email : ${req.body.email}</p>
+            <p>Phone Number: ${req.body.phone_no}</p>
+            <p>Postcode: ${req.body.post_code}</p>
+            <p>Social media: ${req.body.social_media}</p>
+            <p>Message: ${req.body.message}</p>
+        </div>
+            `
+        
+            const mailOptions = {
+                from: `${process.env.EMAIL}`,
+                // ,
+                to:'obadmus912@gmail.com',
+                subject:` ${req.body.subject}`,
+                html:mailbody,
+                
+                
+            };
+        
+        
+            
+                try {
+                    const info = await transporter.sendMail(mailOptions);
+                    console.log('Email Sent' + info.response)
+                    
+                } catch (error) {
+                    console.log(error);
+                    
+                    
+                }
 
-            res.status(200).json({message:"Email Sent"})
-       
-    
-})
+                res.status(200).json({message:"Email Sent"})
+        
+        }
+}
+
+module.exports = allowCors()
+
+
+// export default async (req,res)=>{
+//         res.setHeader('Access-Control-Allow-Credentials', true)
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+
+//         if(req.method == 'GET'){
+//             res.send({status:"Welcome to Nodemailer API"})
+
+//         }else{
+//             let config={
+//                 host: 'smtpout.secureserver.net',
+//         port: 587,
+//         secure: false, // true for 465, false for other ports
+        
+//         tls:{},
+//                 auth:{
+//                     // user: "info@zara-space.co.uk",
+//                     // pass:"D=gM3h$9=k"
+                    
+//                     user: process.env.EMAIL, // generated ethereal user
+//                     pass: process.env.PASSWORD, 
+                    
+//                 }
+//             }
+        
+//             let transporter = nodemailer.createTransport(config);
+        
+//             let mailbody = `
+//             <div>
+//             <p>FullName: ${req.body.name} </p>
+//             <p>Email : ${req.body.email}</p>
+//             <p>Phone Number: ${req.body.phone_no}</p>
+//             <p>Postcode: ${req.body.post_code}</p>
+//             <p>Social media: ${req.body.social_media}</p>
+//             <p>Message: ${req.body.message}</p>
+//         </div>
+//             `
+        
+//             const mailOptions = {
+//                 from: `${process.env.EMAIL}`,
+//                 // ,
+//                 to:'obadmus912@gmail.com',
+//                 subject:` ${req.body.subject}`,
+//                 html:mailbody,
+                
+                
+//             };
+        
+        
+            
+//                 try {
+//                     const info = await transporter.sendMail(mailOptions);
+//                     console.log('Email Sent' + info.response)
+                    
+//                 } catch (error) {
+//                     console.log(error);
+                    
+                    
+//                 }
+
+//                 res.status(200).json({message:"Email Sent"})
+        
+//         }
+// }
 
 
 // const PORT = process.env.PORT || 5000
@@ -153,13 +222,3 @@ app.post('/',cors(),async (req,res)=>{
 // })
 
 // pass:'kvowmiaahpwvwzff'
-
-
- // {
-        //     "src":"/sendmail",
-        //     "methods":[
-        //         "POST",
-        //         "GET"
-        //     ],
-        //     "dest":"api/server.js"
-        // }
