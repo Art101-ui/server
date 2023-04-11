28b94bac10b8887240dc151031889f2497f7ecd3
Badmus_Farouk1 <72664875+Art101-ui@users.noreply.github.com>
2023-04-11 11:22:37 +0100

    remove headers

--- api/server.js
+++ api/server.js
@@ -92,11 +92,8 @@ export default async (req,res)=>{
 
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-      )
+   
+    
 
     if(req.method == 'GET'){
         res.send({status:"Welcome to Nodemailer API"})
